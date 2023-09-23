353f7f6cda30e2a900d34115bdcc3912f99bf128
crowforkotlin <linzepeng@listentech.com.cn>
2023-09-23 15:10:35 +0800

    ◉ 更新Bytes

--- lib_base/src/main/kotlin/com/crow/base/Main.kt
+++ lib_base/src/main/kotlin/com/crow/base/Main.kt
@@ -1,12 +1,9 @@
 package com.crow.base
 
-import com.crow.base.ext.fromLong64
-import java.nio.ByteBuffer
-import kotlin.system.measureNanoTime
 
 fun main() {
 
-    val byteArray = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
+//    val byteArray = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
     // 预热
     /*
           for (i in 1..10000) {
